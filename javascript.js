e7cd2dc86c70b9c3d83760fb0eb52e39044f1d86
Robin Nymoen <robin@nymoen.se>
2025-05-08 10:03:01 +0200

    Ladde till funktioner för att rita ut punkt och linjelager på kartan

--- javascript.js
+++ javascript.js
@@ -1,24 +1,15 @@
-// ESRI moduler deklareras
-let pointLayer;
-let Graphic, Point, SimpleMarkerSymbol;
-
 require([
   "esri/Map",
   "esri/views/MapView",
   "esri/Graphic",
   "esri/layers/GraphicsLayer",
   "esri/symbols/SimpleMarkerSymbol",
-  "esri/geometry/Point"
-], function(Map, MapView, _Graphic, GraphicsLayer, _SimpleMarkerSymbol, _Point) {
+  "esri/geometry/Point",
+  "esri/geometry/Polyline",
+  "esri/symbols/SimpleLineSymbol"
+], function(Map, MapView, Graphic, GraphicsLayer, SimpleMarkerSymbol, Point, Polyline, SimpleLineSymbol) {
 
-  // Spara referenserna till modulerna globalt
-  Graphic = _Graphic;
-  Point = _Point;
-  SimpleMarkerSymbol = _SimpleMarkerSymbol;
-
-  const map = new Map({
-    basemap: "streets"
-  });
+  const map = new Map({ basemap: "streets" });
 
   const view = new MapView({
     container: "viewDiv",
@@ -27,56 +18,121 @@ require([
     zoom: 13
   });
 
-  pointLayer = new GraphicsLayer();
-  map.add(pointLayer);
+  const layers = {}; // Objekt som kan spara flera lager
 
-  getPointsData();
+  // Lägger till lager med funktioner
+  addPointLayer("badplatser", "JSON/badplatser.json", "blue");
+  addPointLayer("idrott_motion", "JSON/idrott_motion.json", "yellow");
+  addPointLayer("lekplatser", "JSON/lekplatser.json", "orange");
+  addPointLayer("livraddningsutrustning", "JSON/livraddningsutrustning.json", "purple");
+  addPointLayer("offentliga_toaletter", "JSON/offentliga_toaletter.json", "cyan");
+  addPointLayer("papperskorgar", "JSON/papperskorgar.json", "black");
+  addPointLayer("pulkabackar", "JSON/pulkabackar.json", "indigo");
+  addPointLayer("rastplatser", "JSON/rastplatser.json", "teal");
+  addPointLayer("spontanidrott", "JSON/spontanidrott.json", "magenta");
+  addPointLayer("utegym", "JSON/utegym.json", "green");
+  getPathsData();
+
+  // Funktion som hämtar JSON-data
+  async function fetchData(file) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) throw new Error(`Failed to fetch ${file}: ${response.statusText}`);
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Fuktion för att spaka point-lager
+  async function addPointLayer(layerName, dataFile, color) {
+    const layer = new GraphicsLayer();
+    layers[layerName] = layer;
+    map.add(layer);
+
+    const data = await fetchData(dataFile);
+    if (data) {
+      showPoints(layer, data, color);
+    }
+  }
+
+  // Funktion som visar punkter på lager
+  function showPoints(layer, data, color) {
+    layer.removeAll();
+
+    data.features.forEach(feature => {
+      const coords = feature.geometry.coordinates;
+      const point = new Point({
+        longitude: coords[0],
+        latitude: coords[1]
+      });
+
+      const symbol = new SimpleMarkerSymbol({
+        style: "circle",
+        color: color,
+        size: 8,
+        outline: {
+          color: "white",
+          width: 1
+        }
+      });
+
+      const graphic = new Graphic({
+        geometry: point,
+        symbol: symbol
+      });
+
+      layer.add(graphic);
+    });
+  }
+
+  // Funktion som läser data från JSON-fil för att sedan rita ett line-lager
+  async function getPathsData() {
+    const pathLayer = new GraphicsLayer();
+    layers["Paths"] = pathLayer; // Lagrar det som ett lager med namnet "Paths" i layers-objektet
+    map.add(pathLayer);
+
+    const data = await fetchData("JSON/motionsspar.json");
+    if (data) {
+        showPaths(pathLayer, data);
+    }
+}
+
+  // Funktion för att rita ut linjer på lager
+  function showPaths(layer, data) {
+    layer.removeAll();
+
+    data.features.forEach(feature => {
+      const coords = feature.geometry.coordinates;
+
+      const line = new Polyline({
+        paths: [coords],
+        spatialReference: { wkid: 4326 }
+      });
+
+      const symbol = new SimpleLineSymbol({
+        color: "blue",
+        width: 3
+      });
+
+      const graphic = new Graphic({
+        geometry: line,
+        symbol: symbol
+      });
+
+      layer.add(graphic);
+    });
+  }
+
+  // Funktion som tar bort lager på kartan
+  function deleteLayer(layerName) {
+    const layer = layers[layerName];
+    if (layer) {
+        map.remove(layer);
+        delete layers[layerName]; // Tar brot referensen till lagret i layers-objektet
+        console.log(`Layer ${layerName} has been deleted.`);
+    } else {
+        console.error(`Layer ${layerName} does not exist.`);
+    }
+  }
 });
-
-// Funktion som läser in JSON-fil
-async function fetchData(file) {
-  const response = await fetch(file);
-  return response.json();
-}
-
-// Laddar in JSON-fil och punkter.
-function getPointsData() {
-  fetchData("JSON/papperskorgar.json").then(data => {
-    showPoints(data);
-  });
-}
-
-// Ritar ut punkter
-function showPoints(data) {
-  if (!pointLayer) return;
-
-  pointLayer.removeAll();
-
-  data.features.forEach(feature => {
-    const coords = feature.geometry.coordinates;
-    const props = feature.properties;
-
-    const point = new Point({
-      longitude: coords[0],
-      latitude: coords[1]
-    });
-
-    const symbol = new SimpleMarkerSymbol({
-      style: "circle",
-      color: "red",
-      size: 8,
-      outline: {
-        color: "white",
-        width: 1
-      }
-    });
-
-    const graphic = new Graphic({
-      geometry: point,
-      symbol: symbol,
-      attributes: props,
-    });
-
-    pointLayer.add(graphic);
-  });
-}
